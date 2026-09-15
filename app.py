9b66911abddcecfd517bafb51fb1eedbb389a761
Anushaaaaa <anusha.verma029@gmail.com>
2026-09-16 05:14:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1562,55 +1562,6 @@ if __name__ == "__main__":
 
     init_db()
 
-        connection = get_db_connection()
-    cursor = connection.cursor()
-
-    admin = cursor.execute(
-        "SELECT id FROM users WHERE email = ?",
-        ("sharma@campus.edu.in",)
-    ).fetchone()
-
-       connection = get_db_connection()
-    cursor = connection.cursor()
-
-    admin = cursor.execute(
-        "SELECT id FROM users WHERE email = ?",
-        ("sharma@campus.edu.in",)
-    ).fetchone()
-
-   if admin:
-    cursor.execute(
-        """
-        UPDATE users
-        SET password = ?, name = ?, course = ?, year = ?, interests = ?
-        WHERE email = ?
-        """,
-        (
-            generate_password_hash("admin123"),
-            "Ms. Sharma",
-            "Science",
-            10,
-            "Teaching,Science",
-            "sharma@campus.edu.in"
-        )
-    )
-else:
-    cursor.execute(
-        """
-        INSERT INTO users
-        (name, email, password, course, year, interests)
-        VALUES (?, ?, ?, ?, ?, ?)
-        """,
-        (
-            "Ms. Sharma",
-            "sharma@campus.edu.in",
-            generate_password_hash("admin123"),
-            "Science",
-            10,
-            "Teaching,Science"
-        )
-    )
-
     print("---------------------------------------")
     print("       CampusCue Backend")
     print("---------------------------------------")
