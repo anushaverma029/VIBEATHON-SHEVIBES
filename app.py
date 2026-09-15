b3d8c5aae6047a9def0a6f5ffdaf9e32dd75cce1
Anushaaaaa <anusha.verma029@gmail.com>
2026-09-16 03:57:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1569,8 +1569,6 @@ if __name__ == "__main__":
     print("Server: http://127.0.0.1:5000")
     print("---------------------------------------")
 
-    app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
-    )
+    import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
